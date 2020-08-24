fdc9f2e0dc0638210638c8a4675b5e8813ffcb5d
stevenchendan <qwe16165850@gmail.com>
2020-08-24 20:20:37 +1000

    add babel-loader rules to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,19 @@ module.exports = {
   entry: {
     main: './main.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            preset: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   mode: "development",
   optimization: {
     minimize: false
